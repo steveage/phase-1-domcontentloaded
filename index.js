d99c7f78219571246aa5324cd2bc67bb7fa0a95b
Mariusz <mariuszbia@yahoo.com>
2022-05-01 22:59:47 -0500

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", replaceParagraphText);
+
+function replaceParagraphText() {
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+}
